7805f873c1f307411ab4c3497802b7b53058bf9a
bryonlhobbs <99457275+bryonlhobbs@users.noreply.github.com>
2022-05-14 15:06:29 -0500

    Module 10 Challenge

--- scraping.py
+++ scraping.py
@@ -5,7 +5,6 @@ import pandas as pd
 import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -19,7 +18,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere": hemisphere(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +96,34 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # Build the image list
+    for i in range(4,11,2):
+        hemispheres = {}
+        full_image_elem = browser.find_by_tag('a')[i]
+        full_image_elem.click()
+        mars_html = browser.html
+        mars_img_soup = soup(mars_html, 'html.parser')
+        img_url_rel = mars_img_soup.find_all('a', target='_blank')[2].get('href')
+        img_url = f'{url}{img_url_rel}'
+        title = mars_img_soup.find('h2')
+        hemispheres['img_url'] = img_url
+        hemispheres['title'] = title.text
+        hemisphere_image_urls.append(hemispheres['img_url'])
+        browser.back()
+
+    return hemisphere_image_urls
+
+
 
 if __name__ == "__main__":
 
